5b42fb8c34be18a14b2b7f280a0ae85d6ba1ed24
WIN10PRO <WIN10PRO@DESKTOP-34KC35S>
2022-09-08 18:38:08 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jose Alejandro Favila Salazar");
+            Console.WriteLine("Mi Edad es de 24");
+            Console.WriteLine("Soy de Mexicali");
+        }
+    }
+}
